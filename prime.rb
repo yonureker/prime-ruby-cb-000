0d285263b2b28520b7a41c1fb6640476fa07385a
Onur Eker <yonureker@gmail.com>
2017-12-22 17:34:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(number)
-  (2..number - 1).all? do |x|
-    number % x != 0 
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
   end
 end
